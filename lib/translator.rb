f19468b8923db026390339a4024e8e4df764ee0f
memcmahon <mcmahon.meganelizabeth@gmail.com>
2017-12-06 10:00:08 -0700

    Update to pass Iteration 1

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,13 +28,23 @@ class Translator
                     "x" => "-..-",
                     "y" => "-.--",
                     "z" => "--..",
+                    "1" => ".----",
+                    "2" => "..---",
+                    "3" => "...--",
+                    "4" => "....-",
+                    "5" => ".....",
+                    "6" => "-....",
+                    "7" => "--...",
+                    "8" => "---..",
+                    "9" => "----.",
+                    "0" => "-----",
                     " " => " "}
   end
 
   def eng_to_morse(text)
     # require 'pry'; binding.pry
     text.split(//).map do |character|
-      dictionary.fetch(character)
+      dictionary.fetch(character.downcase)
     end.join
   end
 end
--- test/translator_test.rb
+++ test/translator_test.rb
@@ -9,5 +9,7 @@ class TranslatorTest < Minitest::Test
     translator.eng_to_morse("hello world")
 
     assert_equal "......-...-..--- .-----.-..-..-..", translator.eng_to_morse("hello world")
+    assert_equal "......-...-..--- .-----.-..-..-..", translator.eng_to_morse("Hello World")
+    assert_equal "-......-.. .-.-.. ...-- ..........--....", translator.eng_to_morse("There are 3 ships")
   end
 end
